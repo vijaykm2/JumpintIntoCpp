de062ad680369dc2e06a5b0c855a3b81d9b2b698
vijay <vijay38576@gmail.com>
2020-04-19 00:12:13 -0500

    initial commit

---
+++ ch5/for.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(long i = 0; i <1234567890; i++){
+        cout << "i squared is "<< i << " = "<<i*i <<"\n";
+    }
+}
---
+++ ch5/while.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long i = 1;
+    while (i <1000000000000){
+        cout << i<< "\n";
+        i *= 2;
+    }
+}
