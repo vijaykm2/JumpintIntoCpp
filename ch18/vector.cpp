baf7623823968eab02d8272ee31f133171b82660
vijay <vijay38576@gmail.com>
2020-05-11 00:09:09 -0500

    initial commit

--- ch18/vector.cpp
+++ ch18/vector.cpp
@@ -6,28 +6,29 @@
 #include<vector>
 #include<iostream>
 #include "map"
+
 using namespace std;
 
-int main(){
-        vector<int> a_vector(10);
-        cout << a_vector.size();
-        for(int i = 0;i<1000;i++){
-            a_vector.push_back(i);
-        }
-        cout << a_vector.size();
-        for(vector<int>::iterator itr = a_vector.begin(), end = a_vector.end(); itr!=end; ++itr){
-            cout<<*itr<<" ";
-        }
-        cout << "\n"<< a_vector.size();
-        map<string, string> name_email;
-        name_email["vijay"]="vijay@maail.com";
-        name_email["vijay2"]="vijay2@mail.com";
-        cout<< name_email["vijay"];
+int main() {
+    vector<int> a_vector(10);
+    cout << a_vector.size();
+    for (int i = 0; i < 1000; i++) {
+        a_vector.push_back(i);
+    }
+    cout << a_vector.size();
+    for (vector<int>::iterator itr = a_vector.begin(), end = a_vector.end(); itr != end; ++itr) {
+        cout << *itr << " ";
+    }
+    cout << "\n" << a_vector.size();
+    map<string, string> name_email;
+    name_email["vijay"] = "vijay@maail.com";
+    name_email["vijay2"] = "vijay2@mail.com";
+    cout << name_email["vijay"];
     name_email.erase("vijay");
-    cout<< name_email["vijay"];
+    cout << name_email["vijay"];
     cout << "\n mapitr \n";
-    for(map<string, string>:: iterator itr = name_email.begin(), end= name_email.end(); itr!= end; ++itr){
-        cout << "key = "<< itr ->first <<" , value = "<< itr ->second<<endl;
+    for (map<string, string>::iterator itr = name_email.begin(), end = name_email.end(); itr != end; ++itr) {
+        cout << "key = " << itr->first << " , value = " << itr->second << endl;
     }
 
 
